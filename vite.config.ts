8015ec33c29da5f331047cf42699c3456feb6709
dlx <daydayfay05@gmail.com>
2025-08-31 03:32:02 +0200

    chore: GH Pages setup (trauerkloss)

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,4 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-/** Für GitHub Pages:
- *  - User/Org Page (username.github.io) => base "/"
- *  - Project Page (username.github.io/<repo>) => base "/<repo>/"
- *  Wir setzen base über ENV (VITE_BASE), lokal bleibt "/".
- */
 const base = process.env.VITE_BASE || "/";
-
-export default defineConfig({
-  base,
-  plugins: [react()],
-  server: { host: true },
-});
+export default defineConfig({ base, plugins: [react()], server: { host: true } });
